fcace70e0ed030d2b039317fd2484c78a527ef78
Natali <natalis.mailbox@gmail.com>
2015-11-08 11:28:58 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+angular.module("multiplicationTable", [])
+.controller("multiplicationCtrl", function($scope, $attrs){
+
+	$scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+	$scope.numbers = function(){
+		var numbers = [];
+		for(var i = 0; i < $scope.numberLimit; i++){
+			numbers[i] = i + 1; 
+		};
+
+		return numbers;
+	}
+
+	$scope.compute = function(a, b){
+		return a*b;
+	}
+
+});
